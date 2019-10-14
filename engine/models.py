9503fc9d6ff835a569242d2acb0b6a46afc68752
Jason Ye <jasonye4273@gmail.com>
2019-10-14 15:23:34 -0400

    Make Game and Profile Models

--- engine/models.py
+++ engine/models.py
@@ -1,16 +1,29 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
-class Player(models.Model):
-	username = models.CharField(primary_key=True, max_length=200, unique=True)
+class Game(models.Model):
 	name = models.CharField(max_length=200)
-	email = models.CharField(max_length=200)
-
-	class Meta:
-		ordering = ('username',)
+	rules = models.TextField(blank=True)
+	active = models.BooleanField(default=False)
 
 	def __str__(self):
-		return self.username
+		return self.name
+
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	recieveEmails = models.BooleanField(default=False)
+	currentPlayer = models.ForeignKey('Player', on_delete=models.SET_NULL, null=True, blank=True)
+
+	def __str__(self):
+		return self.user.username
+
+class Player(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	game = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.user.username
 
 class Condition(models.Model):
 	name = models.CharField(max_length=200)
@@ -30,6 +43,7 @@ class Item(models.Model):
 	name = models.CharField(max_length=200)
 	short_description = models.CharField(max_length=200, default="This is where you enter an item description.", blank=True)
 	long_description = models.TextField(default="This is where you enter long rules text.", blank=True)
+	game = models.ForeignKey(Game, on_delete=models.CASCADE)
 	owners = models.ManyToManyField(Player)
 	triggers = models.ManyToManyField(Condition, through='Trigger', through_fields=('item', 'condition'))
 
@@ -37,6 +51,7 @@ class Item(models.Model):
 		return self.name
 
 class Trigger(models.Model):
+	game = models.ForeignKey(Game, on_delete=models.CASCADE)
 	item = models.ForeignKey(Item, on_delete=models.CASCADE)
 	condition = models.ForeignKey(Condition, on_delete=models.CASCADE)
 	effect = models.ForeignKey(Effect, on_delete=models.CASCADE)
